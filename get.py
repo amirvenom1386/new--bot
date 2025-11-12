965cc3b93fc87de3d159a320ffc6de672a55cd05
amirvenom1386 <141521467+amirvenom1386@users.noreply.github.com>
2025-11-12 14:03:19 +0330

    Update get.py

--- get.py
+++ get.py
@@ -1,7 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 import sqlite3
-conn = sqlite3.connect("E:/projects/news/my_database.db")
+conn = sqlite3.connect("my_database.db")
 cursor = conn.cursor()
 url="https://thehackernews.com/search/label/Cyber%20Attack?max-results=20"
 headers = {"User-Agent": "Mozilla/5.0"}  # برای جلوگیری از بلاک شدن
@@ -25,4 +25,5 @@ for text_link in text_links:
         full_text = "\n".join([p.get_text(strip=True) for p in tag_p])
         cursor.execute("INSERT INTO links (text,url) VALUES (?, ?)", (full_text, url))
         conn.commit()
+
 conn.close()
